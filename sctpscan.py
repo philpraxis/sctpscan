1d7d694f8c5f86b6bcb4c3ff32c6e04def3a4498
Pierre-Olivier Vauboin <po@p1sec.com>
2013-07-05 02:35:28 +0200

    use select not to hang on filtered ports

--- sctpscan.py
+++ sctpscan.py
@@ -4,7 +4,9 @@
 import sys
 import sctp
 import socket
+import select
 import binascii
+import resource
 from IPy import IP
 
 import ports
@@ -16,14 +18,20 @@ def bind_socket(soc, port):
         except socket.error, e:
             if port == ports.default_port:
                 raise e
+            print 'Cant bind mirror port %s, use %s instead' % (port, ports.default_port)
             port = ports.default_port
-            print e
             continue
         break
 
 def main(iprange):
 
+    timeout = 3
+    resource.setrlimit(resource.RLIMIT_NOFILE, (4096, 4096))
+
     for ip in IP(iprange):
+        slist = []
+        opened = closed = filtered = 0
+        print 'Scanning %s' % ip
         for port in ports.sctp_ports:
 
             soc = sctp.sctpsocket_tcp(socket.AF_INET)
@@ -33,14 +41,40 @@ def main(iprange):
                 try:
                     bind_socket(soc, port)
                 except socket.error, e:
-                    print e
+                    print 'Cant bind default port %s, use kernel attributed source port' % ports.default_port
 
+            # Use non blocking sockets + select to scan in parallel
+            soc.settimeout(timeout)
+            soc.setblocking(0)
             try:
                 soc.connect((ip.strNormal(), port))
-                print 'SCTP Port Open: %s' % port
             except socket.error, e:
                 pass
+            slist.append(soc)
+
+        while True:
+            rlist, wlist, xlist = select.select([], slist, [], 1)
+            if not rlist and not wlist and not xlist:
+                break
+            for soc in wlist:
+                try:
+                    # If we can get remote IP/port, then port is opened
+                    name = soc.getpeername()
+                    print 'SCTP Port Open: %s %s' % soc.getpeername()
+                    opened += 1
+                except socket.error, e:
+                    # If we cant get remote IP/port, then port is closed
+                    closed += 1
+                    pass
+                soc.close()
+                slist.remove(soc)
+
+        # Contains filtered ports, or closed ports that were too slow to reply
+        for soc in slist:
+            filtered += 1
             soc.close()
+
+        print 'Results: %s opened, %s closed, %s filtered' % (opened, closed, filtered)
 
 if __name__ == '__main__':
     if not len(sys.argv) == 2:
