0fa74c19d1240ad8ebd1fe18fdb2dac8686d62e1
Ubuntu <ubuntu@vps233780.vps.ovh.ca>
2021-04-16 11:39:19 -0400

    add better logging

--- scan_innerwubs.sh
+++ scan_innerwubs.sh
@@ -2,10 +2,11 @@
 DEST_PORT=${1:-80}
 for I in `cat ip_addr.txt`
 do
-	echo "scan report for network $I on port $DEST_PORT" > results_$I
-	echo -n 'start at ' >> results_$I
-	date >> results_$I
-	sudo ./sctpscan -l `ifconfig ens3 | grep 'inet ' | sed 's/.*inet \(.*\) netmask.*/\1/'` -s -r $I -p $DEST_PORT >> results_$I
-	echo -n 'end at ' >> results_$I
-	date >> results_$I
+	result_file="results_${I}_${DEST_PORT}"
+	echo "scan report for network $I on port $DEST_PORT" > $result_file
+	echo -n 'start at ' >> $result_file
+	date >> $result_file
+	sudo ./sctpscan -l `ifconfig ens3 | grep 'inet ' | sed 's/.*inet \(.*\) netmask.*/\1/'` -s -r $I -p $DEST_PORT >> $result_file
+	echo -n 'end at ' >> $result_file
+	date >> $result_file
 done
